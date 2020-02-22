e0c2ac3a2db95f868e47cd237f2ad643d42620ae
xaecsr <55091870+xaecsr@users.noreply.github.com>
2020-02-22 17:38:18 +0100

    Initial commit

---
+++ script/script.js
@@ -0,0 +1,17 @@
+$('.barra-nivel').each(function() {
+    var valorLargura = $(this).data('nivel');
+    var valorNivel = $(this).html("<span class='valor-nivel'>"+valorLargura+"</span>");
+      $(this).animate({
+          width: valorLargura
+      });
+  });
+
+
+$(document).ready(function(){
+    $(".hide").click(function(){
+      $(".smartphone").hide();
+    });
+    $(".show").click(function(){
+      $(".smartphone").show();
+    });
+  });
